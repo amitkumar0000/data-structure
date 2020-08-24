3d8f55870cbe50c8fd627a04ba9e104658a8fb05
amit kumar <amit.ap1924@gmail.com>
2020-08-24 23:43:53 +0530

    disjoint set(code refactor)

--- src/graph/DisjointSet.kt
+++ src/graph/DisjointSet.kt
@@ -18,7 +18,7 @@ class DisjointSetMap : DisjointSet() {
     }
 
      override fun findSet(data: Int): Node {
-        if(set[data]?.parent?.data!! == data)
+        if(set[data]?.parent?.data == data)
             return set[data]?.parent!!
         return findSet(set[data]?.parent?.data!!)
     }
@@ -27,12 +27,12 @@ class DisjointSetMap : DisjointSet() {
         val p1 = findSet(d1)
         val p2 = findSet(d2)
 
-        if(abs(p1?.rank) > abs(p2?.rank)){
-            p2?.parent = p1
-            p1?.rank = p1?.rank!! - 1
+        if(abs(p1.rank) > abs(p2.rank)){
+            p2.parent = p1
+            p1.rank = p1.rank - 1
         } else {
-            p1?.parent = p2
-            p2?.rank = p2?.rank!! - 1
+            p1.parent = p2
+            p2.rank = p2.rank - 1
         }
     }
 }
