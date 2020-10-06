261e2b7d0c4bd807d315ec4f5da95b571cf41376
amit kumar <amit.ap1924@gmail.com>
2020-10-06 23:29:16 +0530

    construct tree (Inorder + Postorder)

--- src/tree/ConstructTree.kt
+++ src/tree/ConstructTree.kt
@@ -2,6 +2,9 @@ package tree
 
 import java.util.*
 
+/****
+ * Tree can't be constructed if inorder is not given
+ */
 class ConstructTree {
 
     fun constructInorderPreorder(inorder: IntArray, preorder: IntArray): Tree?{
@@ -17,6 +20,22 @@ class ConstructTree {
              root?.left = constructInorderPreorder(inorder.copyOfRange(0, rloc), preorder.copyOfRange(1, rloc+1))
         if(rloc+1 <= inorder.size -1 )
              root?.right = constructInorderPreorder(inorder.copyOfRange(rloc+1, inorder.size), preorder.copyOfRange(rloc+1, preorder.size))
+        return root
+    }
+
+    fun constructInorderPostorder(inorder: IntArray, postorder: IntArray): Tree?{
+        if(inorder.isEmpty() || postorder.isEmpty())
+            return null
+
+        val root: Tree? = Tree(postorder[postorder.size - 1])
+        var rloc = Arrays.binarySearch(inorder, postorder[postorder.size - 1])
+        if(rloc == -1)
+            return root
+
+        if(rloc >= 0)
+            root?.left = constructInorderPostorder(inorder.copyOfRange(0, rloc), postorder.copyOfRange(0, rloc))
+        if(rloc+1 <= inorder.size -1 && rloc <= postorder.size - 1)
+            root?.right = constructInorderPostorder(inorder.copyOfRange(rloc+1, inorder.size), postorder.copyOfRange(rloc, postorder.size - 1))
         return root
     }
 
