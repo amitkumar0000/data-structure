5e03275c55db80c0c88479cc962a2eb62aae01d8
amit kumar <amit.ap1924@gmail.com>
2020-10-08 23:06:20 +0530

    leetcode 144 leetcode 145

--- src/tree/DFS.kt
+++ src/tree/DFS.kt
@@ -1,13 +1,20 @@
 package tree
 
+/**
+ * https://leetcode.com/problems/binary-tree-preorder-traversal/
+ * https://leetcode.com/problems/binary-tree-postorder-traversal/
+ *
+ */
 class DFS {
     internal fun DFS(root: Tree?) {
         println("=== Inorder ===")
         inOrder(root)
         println("\n=== Preoder ===")
-        preOrder(root)
+        preOrder(root).forEach {
+            print("$it")
+        }
         println("\n=== PostOrder ===")
-        postOrder(root)
+        postOrder(root).forEach { print("$it ") }
     }
 
     internal fun inOrder(root: Tree?) {
@@ -20,22 +27,31 @@ class DFS {
 
     }
 
-    internal fun preOrder(root: Tree?) {
+    internal fun preOrder(root: Tree?): List<Int> {
+        val list = arrayListOf<Int>()
         if(root == null)
-            return
-        if(root.`val` != Int.MAX_VALUE)
-            print("${root.`val`} ")
-        preOrder(root.left)
-        preOrder(root.right)
-
+            return list
+        if(root.`val` != Int.MAX_VALUE) {
+            list.add(root.`val`)
+        }
+        preOrder(root.left).forEach {
+            list.add(it)
+        }
+        preOrder(root.right).forEach {
+            list.add(it)
+        }
+        return list
     }
 
-    internal fun postOrder(root: Tree?) {
+    internal fun postOrder(root: Tree?): List<Int> {
+        val list = arrayListOf<Int>()
         if(root == null)
-            return
-        postOrder(root.left)
-        postOrder(root.right)
-        if(root.`val` != Int.MAX_VALUE)
-            print("${root.`val`} ")
+            return list
+        postOrder(root.left).forEach { list.add(it) }
+        postOrder(root.right).forEach { list.add(it) }
+        if(root.`val` != Int.MAX_VALUE) {
+            list.add(root.`val`)
+        }
+        return list
     }
 }
